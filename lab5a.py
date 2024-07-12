71a1742df674570eb0d4daa3b650db8a2993c19f
Rohan Patel <rpatel452@myseneca.ca>
2024-07-12 17:34:15 -0400

    lab5a.py

---
+++ lab5a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Author ID: rpatel452
+
+def read_file_string(file_name):
+   
+    with open(file_name, 'r') as file:
+        return file.read()
+
+def read_file_list(file_name):
+    
+    with open(file_name, 'r') as file:
+        return [line.strip() for line in file]
+
+if __name__ == '__main__':
+    file_name = 'data.txt'
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
